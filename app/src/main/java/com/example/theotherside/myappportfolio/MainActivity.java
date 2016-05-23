1c7164646382129b231f6bfc793e986fd3830267
jakeibeemassa <jac.massa0908@gmail.com>
2016-05-23 15:56:12 -0500

    change implementation click listenner

--- app/src/main/java/com/example/theotherside/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/theotherside/myappportfolio/MainActivity.java
@@ -8,10 +8,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button popular_movies,stock,build_bigger,make_material,go_ubiquitous,capstone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+
+        popular_movies = (Button)findViewById(R.id.popular_movies_button);
+        stock = (Button)findViewById(R.id.stock_hawk_button);
+        build_bigger = (Button) findViewById(R.id.build_it_bigger_button);
+        make_material = (Button) findViewById(R.id.make_material_button);
+        go_ubiquitous = (Button) findViewById(R.id.go_ubiquitous_button);
+        capstone = (Button) findViewById(R.id.capstone_button);
+
+        popular_movies.setOnClickListener(this);
+        stock.setOnClickListener(this);
+        build_bigger.setOnClickListener(this);
+        make_material.setOnClickListener(this);
+        go_ubiquitous.setOnClickListener(this);
+        capstone.setOnClickListener(this);
+
+
 
     }
 
